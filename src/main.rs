bdc4166250b02ffed93890cfad4124faf00528ec
Andrew Murray <darbyguitars@gmail.com>
2019-12-19 15:05:00 -0600

    initialize tcpstream to listen on port 7878 create handler for the connection to print TCP streams to the standart output 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    // listen on the port
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    
+    // for each string that comes in handle the stream
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established");
+
+        handle_connection(stream);
+    }
+}
+
+// this is the stream handler function
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+    
+    // print the stream to the stdout
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
